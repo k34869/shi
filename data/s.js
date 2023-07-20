cdc92599477c4fce27d5142482107fde3e1582d5
k34869 <1838576587@qq.com>
2023-07-20 17:45:14 +0800

    10 commit

--- data/s.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const { argv } = require('process');
-
-let res = fs.readFileSync(argv[2], 'utf8');
-res = res.replace(/https:\/\/cdn\.jsdelivr\.net\/gh\/k34869\/shi/g, '');
-fs.writeFileSync(argv[2], res, 'utf8');
--- data/s.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-. $HOMEBREW_PREFIX/lib/lib.sh
-
-for i in `get_file_all ./ | grep 'index.md'`;do
-    node s.js $i
-done
